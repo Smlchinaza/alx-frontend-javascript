105b541be42c9b3f2eb0cda367d9bea43f0d38fb
Smlchinaza <samchuks898@gmail.com>
2024-05-30 10:10:33 -0700

    ES6 basic

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,11 @@
+
+rt default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const item of Object.values(report.allEmployees)) {
+    allEmployees = [
+      ...allEmployees,
+      ...item,
+    ];
+  }
+  return allEmployees;
+}
